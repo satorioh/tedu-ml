0d24293230251dbe8f77aed60600317351c505eb
Robin.Wang <3Jg$94sbQ@q1>
2024-02-01 09:16:27 +0800

    feat(opencv): add minEnclosingCircle

---
+++ 08_opencv/minEnclosingCircle.py
@@ -0,0 +1,29 @@
+"""
+轮廓拟合：圆形
+"""
+import cv2 as cv
+import numpy as np
+
+img = cv.imread("../dl_data/cloud.png")
+cv.imshow("img", img)
+
+# 转换为灰度图
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+# 二值化
+ret, binary = cv.threshold(gray, 127, 255, cv.THRESH_BINARY)
+cv.imshow('binary', binary)
+
+# 查找轮廓
+contours, hierarchy = cv.findContours(binary, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+print('轮廓数量：', len(contours))
+
+# 轮廓拟合
+(x, y), radius = cv.minEnclosingCircle(contours[0])
+center = (int(x), int(y))
+radius = int(radius)
+res = cv.circle(img, center, radius, (0, 0, 255), 2)
+cv.imshow('res', res)
+
+cv.waitKey()
+cv.destroyAllWindows()
