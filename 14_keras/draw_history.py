05c2892ba3e19710bedba0e52afadb4f4682a106
Robin.Wang <3Jg$94sbQ@q1>
2024-02-29 15:29:17 +0800

    feat(keras): add reuters multiclass classification

---
+++ 14_keras/draw_history.py
@@ -0,0 +1,28 @@
+"""
+绘制训练和验证过程中的loss曲线和accuracy曲线
+"""
+import matplotlib.pyplot as plt
+
+
+def show_history(history):
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    epochs = range(1, len(loss) + 1)
+    plt.figure(figsize=(12, 4))
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs, loss, 'bo', label='Training loss')
+    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs, acc, 'bo', label='Training acc')
+    plt.plot(epochs, val_acc, 'b', label='Validation acc')
+    plt.title('Training and validation accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    plt.show()
