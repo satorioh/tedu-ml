9f63d4f052afb0ce8d755f960e0f8ae7d3981d24
Robin.Wang <3Jg$94sbQ@q1>
2024-02-29 16:48:27 +0800

    feat(keras): add boston housing regression

---
+++ 14_keras/k_fold.py
@@ -0,0 +1,22 @@
+k = 4
+num_val_samples = len(train_data) // k
+num_epochs = 500
+all_mae_histories = []
+for i in range(k):
+    print(f"Processing fold #{i}")
+    val_data = train_data[i * num_val_samples: (i + 1) * num_val_samples]
+    val_targets = train_targets[i * num_val_samples: (i + 1) * num_val_samples]
+    partial_train_data = np.concatenate(
+        [train_data[:i * num_val_samples],
+         train_data[(i + 1) * num_val_samples:]],
+        axis=0)
+    partial_train_targets = np.concatenate(
+        [train_targets[:i * num_val_samples],
+         train_targets[(i + 1) * num_val_samples:]],
+        axis=0)
+    model = build_model()
+    history = model.fit(partial_train_data, partial_train_targets,
+                        validation_data=(val_data, val_targets),
+                        epochs=num_epochs, batch_size=16, verbose=0)
+    mae_history = history.history["val_mae"]
+    all_mae_histories.append(mae_history)
