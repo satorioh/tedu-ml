9e0d321a5388ed8a20c821a90ae444d2d400ec57
Robin.Wang <3Jg$94sbQ@q1>
2024-03-21 15:45:09 +0800

    feat(yolo): add train.py

--- 15_yolo/food_project/dataset_split.py
+++ 15_yolo/food_project/dataset_split.py
@@ -4,7 +4,9 @@ import random
 
 # 获取images文件夹下所有图片的文件名
 image_dir = './dataset/images'
-image_files = [f for f in os.listdir(image_dir) if os.path.isfile(os.path.join(image_dir, f))]
+image_files = [f for f in os.listdir(image_dir) if os.path.isfile(os.path.join(image_dir, f)) and f.endswith('.jpg')]
+label_dir = './dataset/labels'
+label_files = [f for f in os.listdir(label_dir) if os.path.isfile(os.path.join(label_dir, f)) and f.endswith('.txt')]
 
 # 随机打乱文件名
 random.shuffle(image_files)
@@ -20,11 +22,21 @@ test_files = image_files[int(total * (train_ratio + val_ratio)):]
 os.makedirs(f'{image_dir}/train', exist_ok=True)
 os.makedirs(f'{image_dir}/val', exist_ok=True)
 os.makedirs(f'{image_dir}/test', exist_ok=True)
+os.makedirs(f'{label_dir}/train', exist_ok=True)
+os.makedirs(f'{label_dir}/val', exist_ok=True)
+os.makedirs(f'{label_dir}/test', exist_ok=True)
 
 # 将图片复制到对应的文件夹下
 for f in train_files:
     shutil.copy(os.path.join(image_dir, f), f'{image_dir}/train')
+    # 找到同名的label_file并复制到对应的文件夹下
+    label_file = f.replace('.jpg', '.txt')
+    shutil.copy(os.path.join(label_dir, label_file), f'{label_dir}/train')
 for f in val_files:
     shutil.copy(os.path.join(image_dir, f), f'{image_dir}/val')
+    label_file = f.replace('.jpg', '.txt')
+    shutil.copy(os.path.join(label_dir, label_file), f'{label_dir}/val')
 for f in test_files:
     shutil.copy(os.path.join(image_dir, f), f'{image_dir}/test')
+    label_file = f.replace('.jpg', '.txt')
+    shutil.copy(os.path.join(label_dir, label_file), f'{label_dir}/test')
